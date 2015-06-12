6f4a63d8d97b1e4dbdabadb005d0e561b842594e
AceMaridiam <andermation@gmail.com>
2015-06-11 21:33:03 -0700

    transform

--- 02/challenge_transform.py
+++ 02/challenge_transform.py
@@ -1,7 +1,7 @@
 # define the input data
 data_list = [
-    {"id": 123, "name": "abc"},
-    {"id": 456, "name": "def"}
+    {"id": 123, "name": "abc", "phone": "555.555.5555"},
+    {"id": 456, "name": "def", "phone": "444.444.4444"}
 ]
 
 # define the function that we will call later
@@ -10,11 +10,18 @@ def transform(data_list_input):
 
     # DO YOUR WORK HERE
     # LOOP THROUGH ALL ITEMS IN data_list
-    # EXTRACT id from each extracted item
-    # CREATE an output item that is a new Dictionary/Object
-    # COPY all properties except id from the original item into the new one
-    # ADD the new item to data_dictionary using the id as the key.
+    for item in data_list_input:
+        # EXTRACT id from each extracted item
+        id = item["id"]
+        # CREATE an output item that is a new Dictionary/Object
+        output_item = {}
 
+        for key in item.keys():
+            if key != "id":
+                output_item[key] = item[key]
+        # COPY all properties except id from the original item into the new one
+        # ADD the new item to data_dictionary using the id as the key.
+        data_dictionary_output[id] = output_item
     # INCORRECT NAIVE ATTEMPT WITHOUT LOOPS OR VARIABLES
     # data_dictionary_output[data_list_input[0]["id"]] = {"name": data_list_input[0]["name"]}
     # data_dictionary_output[data_list_input[1]["id"]] = {"name": data_list_input[1]["name"]}
