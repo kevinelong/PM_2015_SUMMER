258d8fb746e8ec7d77262073a8dcc5bc1c6eb331
Dadsaster <olliebradley76.com>
2015-06-11 18:12:42 -0700

    This is my phone book

---
+++ StudentWork/OliverBradley/phonebook.py
@@ -0,0 +1,33 @@
+__author__ = 'Oliver'
+
+# Create a dictionary of dictionaries to hold your data.
+phonebook = {}
+
+
+def add():
+    # Function for adding entries
+    pass
+
+
+def change():
+    # Function to change entries
+    pass
+
+
+def delete():
+    # Function to delete entries
+    pass
+
+
+def search():
+    # Function to search for entries
+    pass
+
+
+while True:
+    choice = raw_input("press 1 to search, 2 to add....")
+    if choice == '1':
+        search()
+    elif choice == '2':
+        add()
+    # The rest of the menu code here
