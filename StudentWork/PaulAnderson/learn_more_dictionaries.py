ffb8c97c4da449b24e86bab2e90891f9b821de22
AceMaridiam <andermation@gmail.com>
2015-06-11 21:28:56 -0700

    breakdown of how to add to a dictionary

---
+++ StudentWork/PaulAnderson/learn_more_dictionaries.py
@@ -0,0 +1,12 @@
+favorites = {}
+favorites["color"] = "blue"
+favorites["number"] = 4
+
+# This is the same as the three lines above
+favorites = {
+    "color": "blue",
+    "number": 4,
+}
+
+print "My favorite color is " + favorites["color"]
+print "My favorite number is " + str(favorites["number"])
