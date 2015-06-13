ec3fe92580e8acfb51dec877e2edc1b3aadf12fa
Dadsaster <olliebradley76.com>
2015-06-12 19:04:41 -0700

    completed phonebook

--- StudentWork/OliverBradley/phonebook.py
+++ StudentWork/OliverBradley/phonebook.py
@@ -32,12 +32,10 @@ def change():
                     phonebook[key]["name"] = edit_name
                     phonebook[edit_key[1].lower()] = phonebook.pop(pick_name)
                     print ("Name changed successfully!")
-                    print phonebook
                 elif edit_choice == "2":
                     edit_phone = raw_input("What do you want to change the phone number to? ")
                     phonebook[key]["phone"] = edit_phone
                     print ("Phone number changed successfully!")
-                    print phonebook
                 else:
                     print ("That is not a valid choice!")
                     change()
@@ -48,12 +46,37 @@ def delete():
     delete_name = raw_input("Please enter the last name of the person you want to remove: ").lower()
     print delete_name
     phonebook.pop(delete_name, None)
+    print ("Record successfully deleted!")
 
 
 def search():
     # Function to search for entries
-    pass
-
+    search_choice = raw_input("Enter 1 to search by last name, 2 to search by first name or 3 to search by phone number")
+    if search_choice == "1":
+        search_last = raw_input("Enter last name to search: ").lower()
+        if search_last not in phonebook:
+            print ("That name does not exist!")
+            search()
+        else:
+            print ("Found record!")
+            print phonebook[search_last]
+    elif search_choice == "2":
+        search_first = raw_input("Enter first name to search: ").lower()
+        for key in phonebook:
+            first_match = phonebook[key]["name"].split()
+            if first_match[0].lower() == search_first:
+                print ("Found record!")
+                print phonebook[key]
+    elif search_choice == "3":
+        search_phone = raw_input("Enter phone number to search: ")
+        for key in phonebook:
+            phone_match = phonebook[key]["phone"]
+            if phone_match == search_phone:
+                print ("Found record!")
+                print phonebook[key]
+    else:
+        print ("That isn't a valid choice!")
+        search()
 
 while True:
     choice = raw_input("press 1 to search, 2 to add, 3 to change, 4 to delete and 5 to exit: ")
