4fcba6cf4676e061c45393de883036e9c0f5ac10
Dadsaster <olliebradley76.com>
2015-06-12 19:25:19 -0700

    finished transform?

--- StudentWork/OliverBradley/transform.py
+++ StudentWork/OliverBradley/transform.py
@@ -11,11 +11,19 @@ def transform(data_list_input):
     # DO YOUR WORK HERE
 
     # LOOP THROUGH ALL ITEMS IN data_list
+    for item in data_list:
         # EXTRACT id from each extracted item
-        # CREATE an output item that is a new Dictionary/Object
-        # COPY all properties except id from the original item into the new one
-        # ADD the new item to data_dictionary using the id as the key.
 
+        tag = item["id"]
+        # CREATE an output item that is a new Dictionary/Object
+        out = {}
+        # COPY all properties except id from the original item into the new one
+        out["name"] = item["name"]
+        out["phone"] = item["phone"]
+
+        # ADD the new item to data_dictionary using the id as the key.
+        data_dictionary_output[tag] = out
+        print data_dictionary_output
     # INCORRECT NAIVE ATTEMPT WITHOUT LOOPS OR VARIABLES
     # data_dictionary_output[data_list_input[0]["id"]] = {"name": data_list_input[0]["name"]}
     # data_dictionary_output[data_list_input[1]["id"]] = {"name": data_list_input[1]["name"]}
