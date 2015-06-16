b0de8c02e316e3e06659836977e8d12a96f75140
Reina B Abolofia <purple4reina@gmail.com>
2015-06-15 20:54:21 -0700

    Add my own test for if a letter is uppercase
    Also make sure that the testing method is called

--- 03/reverse_a_string.py
+++ 03/reverse_a_string.py
@@ -8,12 +8,10 @@ def test_reverse_a_string():
     assert(reverse_a_string('Reina') == 'anieR')
     assert(reverse_a_string('r e i n a') == 'a n i e r')
 
-test_reverse_a_string()
-
 def swap_a_case(word):
     new_word = ''
     for letter in word:
-        if letter.is_upper():
+        if is_it_upper(letter):
             new_word += letter.lower()
         else:
             new_word += letter.upper()
@@ -21,3 +19,9 @@ def swap_a_case(word):
 
 def test_swap_a_case():
     assert(swap_a_case('ReInA') == 'rEiNa')
+
+def is_it_upper(letter):
+    return letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+test_reverse_a_string()
+test_swap_a_case()
