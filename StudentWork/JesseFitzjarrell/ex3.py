c7a94a317ec11db121faad16086ef6eff36892b1
jesse fitzjarrell <jessecodesportland@gmail.com>
2015-06-15 18:17:55 -0700

    uploaded 4 initial projects This is my first commit with 4 projects using pycharm

---
+++ StudentWork/JesseFitzjarrell/ex3.py
@@ -0,0 +1,38 @@
+__author__ = 'lenny'
+# Jesse Fitzjarrell
+# Ex 3 in learning Python the hard way.
+
+print " I will now count my chickens:"
+
+print "Hens", 25+30/6
+print "Roosters", 100-25*3%4
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
+
+
+# experimenting with operators
+
+print "what does the / symbol do? what is  3/4?", 3/4
+print "what does the % symbol do? what is 3%4?", 3%4
+
+print "what does the / symbol do? what is 6/2?", 6/2
+print "what does the % symbol do? what is 6%2?", 6%2
+
+print "what does the / symbol do what is 6/4?", 6/4
+print "what does the % symbol do what is 6%4?", 6%4
