f02f1c479394d7e305ad65a0f93e8e8be10ecd91
jesse fitzjarrell <jessecodesportland@gmail.com>
2015-06-16 20:27:07 -0700

    practicing working with lists
    it's the first thing I've done without copying it from the web

---
+++ StudentWork/JesseFitzjarrell/In_Class_Examples_6_16.py
@@ -0,0 +1,11 @@
+__author__ = 'lenny'
+#make a function takes a list, loops through list and prints out each element in the list
+
+def number_list(values):
+
+    for value in values:
+        print value
+
+values = [5, 5, 1, 1, 2, 3, 4, 4, 5]
+
+number_list(values)
