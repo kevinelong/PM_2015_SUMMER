7a60b04b0feed930435b807044518dfab9262bb5
kingstephen80 <kingsofpdx@outlook.com>
2015-06-17 21:27:47 -0700

    These are the files that I have worked on tonight

---
+++ StudentWork/Stephen/W3_PDXCG/comma_list.py
@@ -0,0 +1,14 @@
+__author__ = 'stephen'
+#look for number of people in list, add 'and' between list of to people and before last person in list.
+#insert  comma after names of people before the last name in list.
+
+
+p = ['stephen', 'jessica','Ella','Lukas','Andy']
+def add_comma(p):
+    if len(p) == 2
+        p.append("and")
+    elif len(p) >= 2
+        insert , in between the
+    return add_comma(p)
+
+add_comma()
---
+++ StudentWork/Stephen/W3_PDXCG/random_num.py
@@ -0,0 +1,10 @@
+__author__ = 'stephen'
+import random
+import time
+
+def new_numbers():
+    for seconds in range(10):
+       if 0 <= seconds:
+        print random.randint(self)
+
+new_numbers()
