2f804a5dd85b99351ffd5c95cd34daa88cf45d09
sophiaosintsev <sofica05@gmail.com>
2015-06-17 20:54:22 -0700

    Madlib done from a newspaper article
    Exercise done in Code Guild class

---
+++ StudentWork/SophiaOsintsev/madlib.py
@@ -0,0 +1,28 @@
+article = "The "
+
+input = raw_input("Please enter a type of law enforcement. >> ")
+
+article = article + input + " in northeast Germany \n"
+
+input = raw_input ("Please enter another word for 'ran'. >> ")
+article = article + input + " to what they thought was a \n"
+
+input = raw_input ("Please enter a number. >> ")
+article = article + input + " strong men gang, armed with \n"
+
+input = raw_input ("Please enter a type of weapon. >> ")
+article = article + input + " and sticks over the weekend. But much to their \n"
+
+input = raw_input ("Please enter a synonym of surprise. >> ")
+article = article + input + " officers instead found a group of \n"
+
+input = raw_input ("Please enter a type of vegetable. >> ")
+article = article + input + " pickers walking to their lunch break. \n"
+
+print "\n\n"
+print article
+
+"""name = raw_input("Please enter your name: ")
+age = raw_input(Please enter your age: ")
+
+print "Hello %s, you are %s " %(name, age)"""
