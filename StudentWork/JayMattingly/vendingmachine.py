56fa4fd6c0c599c130c5ee8dba8e5b5e913c28eb
Javafab <jgamattingly@gmail.com>
2015-06-19 18:08:57 -0700

    found mistakes, changed purchase transaction
    added connecting statements
    
    added comments

--- StudentWork/JayMattingly/vendingmachine.py
+++ StudentWork/JayMattingly/vendingmachine.py
@@ -2,8 +2,6 @@ import time
 import webbrowser
 
 class Vending_machine(object):
-
-
     def __init__(self, soda=1.50, chips=1.10, candy=0.75, giftcard=5.00): #gives item selection and prices belonging to each
         self.selection_02 = soda                                                 #class starts with $5.00 in giftcard balance
         self.selection_01 = chips
@@ -14,49 +12,47 @@ class Vending_machine(object):
     def check_balance(self):        #checks to see giftcard balance
         self.amount = self.giftcard
         if self.amount >= 0.75:
-            print "Your balance is {}".format(self.amount)
+            print "Your balance is {}".format(self.amount) #prints current balance
             return self.amount
         else:
             print "Balance too low"
 
-    def item_purchase(self):
-        amount = self.check_balance()
-        if amount > self.selection_01:
-            amount -= self.selection_01
-            print "Your balance is now {}".format(amount)
-            return amount
-        elif amount > self.selection_02:
-            amount -= self.selection_02
-            print "Your balance is now {}".format(amount)
-            return amount
-        elif amount > self.selection_03:
-            amount -= self.selection_03
-            print "Your balance is now {}".format(amount)
-            return amount
+    def item_purchase_01(self):                                                            #######################
+        self.amount -= self.selection_01                                                   #                     #
+        print "Thank youf for your purchase! Your balance is now {}".format(self.amount)   #    Functions to     #
+        return self.amount                                                                 #     purchase        #
+                                                                                           #                     #
+    def item_purchase_02(self):                                                            #                     #
+        print "Thank youf for your purchase! Your balance is now {}".format(self.amount)   #       Need to       #
+        return self.amount                                                                 #        shorten      #
+                                                                                           #                     #
+    def item_purchase_03(self):                                                            #        add promos   #
+        print "Thank youf for your purchase! Your balance is now {}".format(self.amount)   #                     #
+        return self.amount                                                                 ##################### #
 
-vending_machine = Vending_machine()
+vending_machine = Vending_machine(object)
 balance = vending_machine.check_balance()
-choice_00 = vending_machine.item_purchase()
+choice_00 = vending_machine.item_purchase
 
 def vending_visit():
     print "Hello, welcome to your friendly nieghborhood vending machine! Take a gander at what we have to offer!"
     time.sleep(0.5)
-    choice = raw_input("Would you like to see your gift card balance? Press Yes or No\n"
+    choice = raw_input("Would you like to see your gift card balance? Type 'bal'.\n"
                     "(Otherwise pick from our many choices! Input your selection accurately, or else!!!)\n"
                     "If you would like a silly bag of chips, please press one!\n"
                     "If you would like a akward tasting soda, please press two!\n"
                     "If you would like a half eaten candy bar, please press three!\n")
 
 
-    if choice == 'y':
+
+
+    if choice == 'bal':
         vending_machine.check_balance()
         return vending_visit()
-    elif choice == 'n':
-        pass
     elif choice == '1':
-        choice_01
-        return vending_visit()
-    elif choice == '2':
+        if  > self.selection_01:
+        ret    amount -= self.selection_01                  urn vending_visit()
+    elif ch    print "Your balance is now {}".format(amount)oice == '2':
         Vending_machine.item_purchase.self.selection_02
         return vending_visit()
     elif choice == '3':
