456c0cffd203eb28c3de3f6111a86e1531d0f02b
Javafab <jgamattingly@gmail.com>
2015-06-19 21:30:55 -0700

    added functionality to add_item to cart, created menu

--- StudentWork/JayMattingly/PMSchoolWork/Shopping_cart.py
+++ StudentWork/JayMattingly/PMSchoolWork/Shopping_cart.py
@@ -1,7 +1,8 @@
+import time
 food_dict = {
-        'poultry': {"eggs": "Dozen", "price": 1.99},
-        'dairy': {"milk": "Gallon", "price": 2.59},
-        'dry': {"flour": "Pound", "price": 3.49}
+        'eggs': {"eggs": "Dozen", "price": 1.99},
+        'milk': {"milk": "Gallon", "price": 2.59},
+        'flour': {"flour": "Pound", "price": 3.49}
 }
 
 class Shopping_cart():
@@ -10,19 +11,34 @@ class Shopping_cart():
         pass
 
     def add_item(self):
-        user_input = []
+        user_cart = []
         user_input = raw_input("What do you want to add?\n")
         if user_input == 'eggs':
-            choice_01 = (food_dict["poultry"])
-        elif user_input == 'dairy':
-            choice_02 = (food_dict["dairy"])
-        elif user_input == 'dry':
-            choice_03 = (food_dict["dry"])
-
-        elif user_input == '3':
-            pass
+                choice_01 = (food_dict["eggs"])
+                user_cart.append(choice_01)
+                print "You\'ve added {}".format(user_cart)
+                self.menu()
+        elif user_input == 'milk':
+                choice_02 = (food_dict["milk"])
+                user_cart.append(choice_02)
+                print in_cart
+                self.menu()
+        elif user_input == 'flour':
+                choice_03 = (food_dict["flour"])
+                user_cart.append(choice_03)
+                print in_cart
+                self.menu()
             
 
+    def menu(self):
+        choice = raw_input("Would you like to check out or continue?: ")
+        if choice == "check out":
+
+            pass
+        else:
+            self.add_item()
+            pass
+        pass
 
 
 
@@ -31,6 +47,11 @@ class Shopping_cart():
     def view_cart(self):
         pass
 
+
 shopping = Shopping_cart()
+print "Choose your item!"
+time.sleep(.5)
+for key in food_dict:
+    print key
 
 shopping.add_item()
