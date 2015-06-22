3247d0de1ae35ab904236330ccc0557642979afa
Reina B Abolofia <purple4reina@gmail.com>
2015-06-22 13:05:19 -0700

    Dict vs Classes to facilitate discussion on when to use each

---
+++ wk04/dictionaires_vs_classes.py
@@ -0,0 +1,30 @@
+# To facilitate a discussion on using dictionaries vs classes to represent the
+# attributes on an object
+
+
+# Using a class
+class Person(object):
+    def __init__(self, name=None, age=None, height=None):
+        self.name = name
+        self.age = age
+        self.height = height  # in inches
+
+person = Person(name='Reina', age=33, height=64)
+print "\033[93m person:  ",  person, "\033[0m"
+
+
+# Using a dictionary
+person = {'name': 'Reina', 'age': 33, 'height': 64}
+print "\033[93m person:  ",  person, "\033[0m"
+
+
+# Sample usage
+def grow_taller(person, growth):
+    if isinstance(person, object):
+        # if person is a class
+        person.height += growth
+    elif isinstance(person, dict):
+        # if person is a dictionary
+        person['height'] += growth
+
+    return person
