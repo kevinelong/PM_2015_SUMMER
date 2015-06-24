6cc427689bd804ed8587a05bb4fa36873193e0ed
Rachel Klein <rachel.leah.klein@gmail.com>
2015-06-23 23:54:17 -0700

    Fixed bug in phonebook with file object program.

--- StudentWork/RachelKlein/file_objects.py
+++ StudentWork/RachelKlein/file_objects.py
@@ -11,16 +11,16 @@ choice = int(raw_input('Do you want to (1) search the phonebook or (2) add an en
 # Appends a phonebook entry the user gives to the phonebook.txt file.
 
 if choice == 2:
-    name = raw_input('What is the name of the person you wish to add?\nPlease format like this: Last name, First name ')
-    phone_num = raw_input('What is the phone numer of the person you wish to add?\n Please format like this: 222-333-4444 ')
+    name = raw_input('What is the name of the person you wish to add?\nPlease format like this: Firstname Lastname > ')
+    phone_num = raw_input('What is the phone number of the person you wish to add?\n Please format like this: 222-333-4444 ')
 
     with open(phonebook, 'a') as updated_phonebook:
         updated_phonebook.write('{},{}\n'.format(name, phone_num))
 
-# Loops through the existing phonebook.txt entries, finds a match for the name, and prints that out.
+# Loops through the existing phonebook.txt entries, finds a match for the name, and prints the name and phone number out.
 
 elif choice == 1:
-    name_requested = raw_input('Whose phone number would you like to access? Please format like this: Last name, First name ')
+    name_requested = raw_input('Whose phone number would you like to access? Please format like this: Firstname Lastname > ')
     with open(phonebook) as searchable_phonebook:
         for line in searchable_phonebook:
             name, phone_num = line.split(',')
