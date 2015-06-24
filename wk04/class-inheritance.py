6763185b764aa3ec1fe5c65aef317c9de149c29a
Reina B Abolofia <purple4reina@gmail.com>
2015-06-24 13:26:38 -0700

    Example of class inheritances
    Showing how class inheritance affects methods

---
+++ wk04/class-inheritance.py
@@ -0,0 +1,22 @@
+class Animal(object):
+
+    def take_for_walk(self):
+        pass
+
+    def feed(self):
+        pass
+
+
+class Dog(Animal):
+
+    def take_for_walk(self):
+        pass
+
+    def give_bath(self):
+        pass
+
+
+class Cat(Animal):
+
+    def take_for_walk(self):
+        pass
