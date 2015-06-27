3dc6184f9db5d45b54db9534e707e3ef7fa01e93
Reina B Abolofia <purple4reina@gmail.com>
2015-06-26 18:57:52 -0700

    Reina's version 2 of Tamagotchi toy

--- wk04/reina-tamagotchi.py
+++ wk04/reina-tamagotchi.py
@@ -1,5 +1,50 @@
 import random
+import sys
 import time
+
+import asciiart
+
+
+def activity(fn):
+    """
+    Decorator for any methods that represent an activity of daily living for an
+    animal
+
+    Deals with any needs including death and sleepiness
+    """
+    def deal_with_needs(animal, *args, **kwargs):
+
+        return_val = fn(animal, *args, **kwargs)
+
+        animal.age += 1
+        animal.sleepiness += 1
+        animal.hunger += 1
+
+        if animal.sleepiness > animal.sleepiness_limit:
+            print '{} fell asleep'.format(animal.name)
+            time.sleep(animal.nap_length)
+            animal.sleepiness = 0
+            animal.excitement = 0
+        if animal.hunger > animal.hunger_warning_limit:
+            print '{} needs to eat or will die of hunger!'.format(animal.name)
+        if animal.excitement < animal.excitement_warning_limit:
+            print '{} needs your attention'.format(animal.name)
+
+        # deal with death situations
+        if animal.hunger > animal.hunger_death_limit:
+            raise DeathException('{} died of hunger'.format(animal.name))
+        elif animal.age > animal.age_limit + random.randint(-2, 2):
+            raise DeathException(
+                '{} lived a happy life and died of old age'.format(animal.name))
+        elif not random.randrange(100):
+            # 1 in 100 chance the animal will just die
+            raise DeathException('{} died randomly'.format(animal.name))
+        elif animal.excitement < animal.excitement_runaway_limit:
+            raise RunAwayException('{} ran away'.format(animal.name))
+
+        return return_val
+
+    return deal_with_needs
 
 
 class Animal(object):
@@ -9,6 +54,7 @@ class Animal(object):
     hunger_warning_limit = 5
     hunger_death_limit = 7
     age_limit = 10
+    excitement_limit = 3
 
     def __init__(self, name):
         self.name = name
@@ -18,53 +64,29 @@ class Animal(object):
         self.age = 0
         print '{} is so excited to go home with you!'.format(self.name)
 
+    @activity
     def feed(self):
         if self.hunger < 3:
             print '{} isn\'t hungry right now'.format(self.name)
         else:
-            print 'Yum! {} ate their food!'.format(self.name)
-            self.hunger -= 1
-            self.sleepiness += 2
-            self.excitement -= 1
-        self.deal_with_needs()
+            print 'Yum! {} ate the food all up!'.format(self.name)
+            self.hunger -= 2
 
+    @activity
     def pet(self):
         if self.hunger > 3:
             print '{} is too hungry to play'.format(self.name)
         else:
             print '{} is having so much fun being petted!'.format(self.name)
-            self.hunger += 1
             self.sleepiness += 2
             self.excitement += 1
-        self.deal_with_needs()
-
-    def print_status(self):
-        print (
-            '{}\'s hunger is {}, sleepiness is {}, and excitement '
-            'is {}'.format(
-                self.name, self.hunger, self.sleepiness, self.excitement)
-        )
-
-    def deal_with_needs(self):
-        if self.sleepiness > self.sleepiness_limit:
-            print '{} fell asleep'.format(self.name)
-            time.sleep(self.nap_length)
-            self.sleepiness = 0
-        if self.hunger > self.hunger_warning_limit:
-            print '{} needs to eat or will die of hunger!'.format(self.name)
-
-        # deal with death situations
-        if self.hunger > self.hunger_death_limit:
-            raise DeathException('{} died of hunger'.format(self.name))
-        elif self.age > self.age_limit + random.randint(-2, 2):
-            raise DeathException(
-                '{} lived a happy life and died of old age'.format(self.name))
-        elif not random.randrange(100):
-            # 1 in 100 chance the animal will just die
-            raise DeathException('{} died randomly'.format(self.name))
 
     def __repr__(self):
         return '{}: {}'.format(self.__class__.__name__, self.name)
+
+    def get_choices(self):
+        return ['pet', 'feed']
+
 
 
 class Dog(Animal):
@@ -75,8 +97,14 @@ class Dog(Animal):
     hunger_death_limit = 7
     age_limit = 12
 
+    @activity
     def give_bath(self):
         pass
+
+    def get_choices(self):
+        parent_choices = super(self.__class__, self).get_choices()
+        dog_choices = ['give_bath']
+        return parent_choices + dog_choices
 
 
 class Cat(Animal):
@@ -87,8 +115,18 @@ class Cat(Animal):
     hunger_death_limit = 8
     age_limit = 20
 
+    @activity
     def play_with_laser_pen(self):
-        pass
+        if self.hunger > self.hunger_limit:
+            print '{} is too hungry to play'.format(self.name)
+        else:
+            self.excitement += 2
+            self.sleepiness += 1
+
+    def get_choices(self):
+        parent_choices = super(self.__class__, self).get_choices()
+        cat_choices = ['play_with_laser_pen']
+        return parent_choices + cat_choices
 
 
 class TamagotchiException(Exception):
@@ -103,6 +141,27 @@ class DeathException(TamagotchiException):
     """
 
 
+class RunAwayException(TamagotchiException):
+    """
+    Exception raised when an animal runs away because it is too bored with you
+    """
+
+
+def list_to_comma(mylist, conjunction='and'):
+    """
+    Given a list of strings, return a comma separated list of those strings
+    """
+    len_mylist = len(mylist)
+    if len_mylist == 0:
+        return ''
+    elif len_mylist == 1:
+        return mylist[0]
+    elif len_mylist == 2:
+        return mylist[0] + ' ' + conjunction + ' ' + mylist[1]
+    else:
+        return ', '.join(mylist[:-1]) + ', ' + conjunction + ' ' + mylist[-1]
+
+
 if __name__ == '__main__':
     pet_type = raw_input(
         'Welcome to the pet store! What type of animal would '
@@ -115,3 +174,23 @@ if __name__ == '__main__':
         pet = globals().pop(pet_type)(name)
     except KeyError:
         pet = Animal(name)
+
+    while True:
+        avail_choices = pet.get_choices()
+        avail_choices_str = list_to_comma(avail_choices, conjunction='or')
+        choice = raw_input(
+            '\nWhat would you like to do with {}?  '
+            '{}\n'.format(name, avail_choices_str))
+
+        try:
+            getattr(pet, choice)()
+        except AttributeError:
+            print '{} is not an available choice'.format(choice)
+        except DeathException as err:
+            print asciiart.death
+            print err
+            sys.exit(0)
+        except RunAwayException as err:
+            print ascciart.run_away
+            print err
+            sys.exit(0)
