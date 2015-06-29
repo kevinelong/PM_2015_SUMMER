a5263f38f836c5e9f2a99c2a38d18d39404440bb
jesse fitzjarrell <jessecodesportland@gmail.com>
2015-06-29 14:57:30 -0700

    starting Learn Python The hard way from scratch and taking notes

---
+++ StudentWork/JesseFitzjarrell/Things_I_Know_About_Python_Examples.py
@@ -0,0 +1,35 @@
+__author__ = 'lenny'
+"""
+Jesse Fitzjarrell
+6-29-15
+Python examples to go with my study of what I know in Python
+"""
+print "practicing print functions"
+print "butter"
+print 30
+print "butter",30
+
+print "practicing printing math"
+print 9+5
+print 8/2
+print 4*2
+
+print "looking at math operators"
+
+print 10/4,"10/4"
+print 10%3,"10%3"
+
+print "Learing about variables"
+
+peaunt_butter = "delicious"
+bacon = "glorious"
+burger = "Fatty Meat"
+pbpb = 5.95
+
+print "the", peaunt_butter, bacon, burger, "is", pbpb,  "at Killer burger"
+
+favorite_food = "Pizza"
+favorite_beverage = "Beer"
+favorite_place = "outdoors"
+
+print "I really like to drink %s and eat %s when I am %s" %(favorite_beverage, favorite_food, favorite_place)
