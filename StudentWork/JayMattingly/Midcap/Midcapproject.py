9adf9fab0fe34c03c99d5499a626fccf1887a488
Javafab <jgamattingly@gmail.com>
2015-06-30 21:30:05 -0700

    fixed selection in main menu
    created mutable dictionary

--- StudentWork/JayMattingly/Midcap/Midcapproject.py
+++ StudentWork/JayMattingly/Midcap/Midcapproject.py
@@ -1,31 +1,17 @@
 import time
 import webbrowser
 
-progress_report = {"gender": "male or female", "age": 24, "Weight": 164, "height": 72}
-
-# class user_stats(object):
-#     def __init__(self):
-#         self.age = raw_input()
-#         self.height
-#         self.weight
-#         self.activity_level
-#         self.heartrate
-
-    # def my_fit_questions(self):
-    #     age = self.age
-    #     while True:
-    #         if age > 16:
-    #             pass
-    #         else:
-#     #             print "It is recommended you be 16 years of age or older to use this program. Thank you."
-#
-# def heartrate():
-#     age = raw_input(int("How old are you?"))
-#     m_hrate = 220 - age
-#     print m_hrate
+progress_report_dict = {
+    'james': {'gender': "male or female", 'age': 24, 'weight': 164, 'height': 72},
+    'smith': {'gender': "male or female", 'age': 24, 'weight': 164, 'height': 72},
+    'gordon': {'gender': "male or female", 'age': 24, 'weight': 164, 'height': 72},
+    'munster': {'gender': "male or female", 'age': 24, 'weight': 164, 'height': 72},
+    'robinson': {'gender': "male or female", 'age': 24, 'weight': 164, 'height': 72}
+}
 
 def response_1():
     choice = raw_input("What would you like to know?\n"
+                       "\n"
                        "1. What's my maximum heart rate for my age?\n"
                        "2. How many calories should I burn daily to reach goals?\n"
                        "3. My Progress Report.\n"
@@ -41,8 +27,7 @@ def response_1():
         webbrowser.open('http://www.freedieting.com/tools/calorie_calculator.htm')
         response_1()
     elif choice == '3':
-        progress()
-        response_1()
+        my_progress()
     elif choice == '4':
         pass
     else:
@@ -62,32 +47,62 @@ def heart_rate():
           "too hard or your heart rate is too high, back off a bit.\n" \
           "".format(lower_range,upper_range)
 
+def my_progress():
+    decision = raw_input("What would you like to do? Select 1 to view a profile or 2 to change a profile.")
+    if decision == '1':
+        search_report()
+        my_progress()
+    elif decision == '2':
+        search_name = raw_input("Input the user's last name of which you'd like to review.")
+        if search_name not in progress_report_dict:
+            print ("The user profile you are searching for does not exist. Please make another selection.")
+            my_progress()
+        else:
+            print "Searching for you selection, this may take a moment...\n"
+            change_report(search_name)
+    print "From whence we left off, here is your profile! {}".format(progress_report_dict[search_name])
 
-def progress():
-    change_info = raw_input("What would you like to update? Gender, Age, Weight, Height?")
-    if change_info not in progress_report:
-        print "I don't understand, please make a choice from the variabls given."
-        change_info
+def search_report():
+    search_name = raw_input("Input the user's last name of which you'd like to review.")
+    print "yay"
+    if search_name in progress_report_dict:
+        print progress_report_dict[search_name]
+
+
+
+def change_report(name):
+    change_info = raw_input("What would you like to update? Gender, Age, Weight, Height?\n")
+    if change_info == 'gender':
+        gender = raw_input('Please enter your updated gender: ')
+        progress_report_dict[name]['gender'] = gender
+    elif change_info == 'age':
+        age = raw_input('Please enter your updated age (Happy Birthday!): ')
+        progress_report_dict[name]['age'] = age
+    elif change_info == 'weight':
+        weight = raw_input('Please enter your updated weight: ')
+        progress_report_dict[name]['weight'] = weight
+    elif change_info == 'height':
+        height  =   raw_input('Please enter your updated height: ')
+        progress_report_dict[name]['height'] = height
     else:
-        for key in progress_report:
-            if key == change_info:
-                gender = raw_input('Please enter your updated gender: ')
-                age = raw_input('Please enter your updated age(Happy Birthday!: ')
-                weight = raw_input('Please enter your updated weight: ')
-                height = raw_input('Please enter your updated height: ')
-                print progress_report
+        print "I don't understand, please make a choice from the variables given.\n"
     print "\n"
-    print "                     _ _ _ _ _ _ _ _\n" \
-          "                    /   _ _ _ _ _ _/\n" \
-          "                   /   /\n" \
-          "                  /   /\n" \
-          "                 /   /\n" \
-          "                /   /   _ _ _  \n" \
-          "               /   /   / _   /   \n" \
-          "              /   /_ _ _ /  /\n" \
-          "             /_ _ _ _ _ _ _/\n"
-    print "\n"
-    response_1()
+    print """
+  ______                             __           _____            __        __
+ /      \                           /  |         /     |          /  |      /  |
+/$$$$$$  |  ______    ______    ____$$ |         $$$$$ |  ______  $$ |____  $$ |
+$$ | _$$/  /      \  /      \  /    $$ |            $$ | /      \ $$      \ $$ |
+$$ |/    |/$$$$$$  |/$$$$$$  |/$$$$$$$ |       __   $$ |/$$$$$$  |$$$$$$$  |$$ |
+$$ |$$$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |      /  |  $$ |$$ |  $$ |$$ |  $$ |$$/
+$$ \__$$ |$$ \__$$ |$$ \__$$ |$$ \__$$ |      $$ \__$$ |$$ \__$$ |$$ |__$$ | __
+$$    $$/ $$    $$/ $$    $$/ $$    $$ |      $$    $$/ $$    $$/ $$    $$/ /  |
+ $$$$$$/   $$$$$$/   $$$$$$/   $$$$$$$/        $$$$$$/   $$$$$$/  $$$$$$$/  $$/
+
+
+
+
+"""
+
 
 
 while True:
@@ -97,12 +112,13 @@ while True:
         print "You'll get the most from your workouts if you're exercising\n" \
           "at the proper exercise intensity for your health and fitness goals.\n"
         response_1()
-
-    else:
+    elif status.lower() in 'bad':
         print "Sorry to hear that, lets try to workout a way into a good time!\n"
         print "You'll get the most from your workouts if you're exercising\n" \
           "at the proper exercise intensity for your health and fitness goals.\n"
         response_1()
+    else:
+        print "You are either good or bad, don't try to get testy with me!"
 
 
 
