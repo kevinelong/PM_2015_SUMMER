1e2fad5e23a2c0118b4348bb95fc56ba9b81c904
jesse fitzjarrell <jessecodesportland@gmail.com>
2015-06-30 21:12:28 -0700

    continuing to work on my Learn Python the hard way and starting my Mini Capstone Phish Project

--- StudentWork/JesseFitzjarrell/Phish_Tour_Planner_Model.py
+++ StudentWork/JesseFitzjarrell/Phish_Tour_Planner_Model.py
@@ -6,8 +6,68 @@ Summer 2015 Phish Tour Planner Model File
 My first real Python file, let's do it.
 """
 
-phish_show_list = {'Bend_night_1':'7/21 Les Schwab Amphitheatre, Bend, OR',
+"""
+phish_show_dictionary = {'Bend_night_1':'7/21 Les Schwab Amphitheatre, Bend, OR',
                    'Bend_night_2':'7/22 Les Schwab Amphitheatre, Bend, OR',
-                   }
+                   'Mountain View':'7/24 Shoreline Amphitheatre, Mountain View, CA',
+                   'Los Angeles': '7/25 Forum, Los Angeles, CA',
+                   'Austin': '7/28 Austin360 Amphitheater, Austin, TX',
+                   'Grand Prarie, TX':'7/29 Verizon Theatre, Grand Prairie, TX',
+                    'Atlanta, GA' :'8/1 Lakewood, Atlanta, GA',
+                    'Tuscaloosa, AL': '8/2 Tuscaloosa Amphitheater, Tuscaloosa, AL',
+                    'Nashville, TN':'8/4 Riverfront Park, Nashville, TN',
+                    'Kansas City, MO':'8/5 Starlight Theatre, Kansas City, MO',
+                    'Cuyahoga Falls, OR':'8/7 Blossom, Cuyahoga Falls, OH',
+                    'East Troy, WI Night 1':'8/8 Alpine Valley, East Troy, WI',
+                    'East Troy, WINight2':'8/9 Alpine Valley, East Troy, WI',
+                    'Philadelphia, PA Night 1':'8/11 The Mann, Philadelphia, PA',
+                    'Philadelphia, PA Night 2':'8/12 The Mann, Philadelphia, PA',
+                    'Raleigh, NC':'8/14 Walnut Creek Amphitheater, Raleigh, NC',
+                    'Columbia, MD Night 1':'8/15 Merriweather Post Pavilion, Columbia, MD',
+                    'Columbia, MN Night 2':'8/16 Merriweather Post Pavilion, Columbia, MD',
+                    'Watkins Glen, NY Night 1':'8/21 Mangaball, Watkins Glen, NY',
+                    'Watkins Glen, NY Night 2':'8/22 Magnaball, Watkins Glen, NY',
+                    'Watkins Glen, NY Night 3':'8/23 Magnaball, Watkins Glen, NY',
+                    'Commerce City, CO Night 1':'9/4 Dick\'s Sporting Goods Park, Commerce City, CO',
+                    'Commerce City, CO Night 2':'9/5 Dick\'s Sporting Goods Park, Commerce City, CO',
+                    'Commerce City, CO Night 3':'9/6 Dick\'s Sporting Goods Park, Commerce City, CO',
+                    }
 
-print phish_show_list
+
+
+
+
+for keys,values in phish_show_dictionary.items():
+    print(keys)
+    print(values)
+"""
+
+phish_show_list = ("Here is a list of Phish's Summer 2015 dates!!!!",
+                    'Bend_night_1 7/21 Les Schwab Amphitheatre, Bend, OR',
+                   'Bend_night_2 7/22 Les Schwab Amphitheatre, Bend, OR',
+                   'Mountain View 7/24 Shoreline Amphitheatre, Mountain View, CA',
+                   'Los Angeles 7/25 Forum, Los Angeles, CA',
+                   'Austin 7/28 Austin360 Amphitheater, Austin, TX',
+                   'Grand Prarie, TX 7/29 Verizon Theatre, Grand Prairie, TX',
+                    'Atlanta, GA 8/1 Lakewood, Atlanta, GA',
+                    'Tuscaloosa, AL 8/2 Tuscaloosa Amphitheater, Tuscaloosa, AL',
+                    'Nashville, TN 8/4 Riverfront Park, Nashville, TN',
+                    'Kansas City, MO 8/5 Starlight Theatre, Kansas City, MO',
+                    'Cuyahoga Falls, OH 8/7 Blossom, Cuyahoga Falls, OH',
+                    'East Troy, WI Night 1 8/8 Alpine Valley, East Troy, WI',
+                    'East Troy, WI Night2 8/9 Alpine Valley, East Troy, WI',
+                    'Philadelphia, PA Night 1 8/11 The Mann, Philadelphia, PA',
+                    'Philadelphia, PA Night 2 8/12 The Mann, Philadelphia, PA',
+                    'Raleigh, NC 8/14 Walnut Creek Amphitheater, Raleigh, NC',
+                    'Columbia, MD Night 1 8/15 Merriweather Post Pavilion, Columbia, MD',
+                    'Columbia, MN Night 2 8/16 Merriweather Post Pavilion, Columbia, MD',
+                    'Watkins Glen, NY Night 1 8/21 Mangaball, Watkins Glen, NY',
+                    'Watkins Glen, NY Night 2 8/22 Magnaball, Watkins Glen, NY',
+                    'Watkins Glen, NY Night 3 8/23 Magnaball, Watkins Glen, NY',
+                    'Commerce City, CO Night 1 9/4 Dick\'s Sporting Goods Park, Commerce City, CO',
+                    'Commerce City, CO Night 2 9/5 Dick\'s Sporting Goods Park, Commerce City, CO',
+                    'Commerce City, CO Night 3 9/6 Dick\'s Sporting Goods Park, Commerce City, CO',
+)
+
+#for elem in phish_show_list:
+ #       print elem
