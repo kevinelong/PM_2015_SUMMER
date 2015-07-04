c198a1d736bc424ddad62f28f7531d2a55e3e86e
Rachel Klein <rachel.leah.klein@gmail.com>
2015-07-04 15:42:13 -0700

    Fixed issue with win condition function in Bagels.

--- StudentWork/RachelKlein/bagels.py
+++ StudentWork/RachelKlein/bagels.py
@@ -9,7 +9,9 @@ def did_player_win(codewords, digits):
     or a computer player has won the game.
     """
 
-    if len(codewords) == digits and set(codewords) == set(["fermi"]):
+    if len(codewords) == digits and set(codewords) == set(["Fermi"]):
+        return True
+    elif len(codewords) == digits and set(codwrods) == set(["fermi"]):
         return True
     else:
         return False
