1bc7c373cbbe8e3f9cd81ece177a23916530d340
Reina B Abolofia <purple4reina@gmail.com>
2015-07-13 22:14:03 -0700

    Shopping list items only clickable once

--- wk06/friday/loops.js
+++ wk06/friday/loops.js
@@ -1,13 +1,40 @@
 var myItems = ['carrots', 'celery', 'flour', 'cereal', 'milk', 'paper towels'];
+var openTag = '<p class="clickable">';
+var closeTag = '<span class="glyphicon glyphicon-remove"></span></p>';
+
 
 for (var i = 0; i < myItems.length; i++) {
-    document.write('<p>' + myItems[i] + '</p>');
+    document.write(openTag + myItems[i] + closeTag);
 }
 
+// item purchased
 $('p').on('click', function(e) {
-    $(this).fadeOut(1300);
+    if ($(this).hasClass('clickable')) {
+        $(this).fadeOut(1300, function () {
+            $(this).css('text-decoration', 'line-through');
+            $(this).find('span').hide();
+        }).fadeIn('slow');
+        $(this).removeClass();
+    }
 });
 
+// dismiss by clicking the x
+$('span').on('click', function() {
+   $(this).parent().fadeOut(1000);
+    return false; // prevents also clicking the parent p tag
+});
+
+// reset
 $('button').on('click', function(e) {
-    $('p').fadeIn(500);
+    $('p').each(function(index){
+        if ($(this).css('text-decoration') === 'line-through') {
+            $(this).hide();
+            $(this).css('text-decoration', '');
+            $(this).find('span').show();
+            $(this).fadeIn('slow');
+        }
+        $(this).attr('class', 'clickable');
+        $(this).show();
+    });
+
 });
