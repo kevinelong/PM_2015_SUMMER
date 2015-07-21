8cdea61e2113976acc8a03b1372b185bb3edc0e6
Dar Wright <elack33@gmail.com>
2015-07-21 13:49:16 -0700

    Finished animated menu assignment

--- StudentWork/DarWright/JavaScript/mon_07_20/animated_menu.js
+++ StudentWork/DarWright/JavaScript/mon_07_20/animated_menu.js
@@ -1,46 +1,32 @@
 $(document).ready(function() {
-    //get element by iD, and then make a click function for it? maybe?
-
-    //$('li').on('click', function() {
-    //    console.log('One click!');
-    //    var par = $(this).parent().attr('id');
-    //    console.log(par);
-    //    if ($(this).parent().attr('id') === 'available' ) {
-    //        var id = $(this).attr('id');
-    //        $('#ordered').append('<li  class="item" id=new' + id + '>' + $(this).text() + '</li>');
-    //        id = '#' + id;
-    //        console.log('click from left', id);
-    //        $(id).remove();
-    //        }
-    //    else if ($(this).parent().attr('id') === 'ordered' ){
-    //        var id = $(this).attr('id');
-    //        console.log('click on the right side');
-    //        $('#available').append('<li  class="item" id=old' + id + '>' + $(this).text() + '</li>');
-    //        id = '#' + id;
-    //        console.log('click from right', id);
-    //        $(id).remove();
-    //    }
-    //});
-
     $('#menu').on('click', function (event) {
         var target = $(event.target);
         if (target.hasClass('item')) {
+            target.addClass('clicked');
             var id = target.attr('id');
             if (target.parent().attr('id') === 'available' ) {
-                $('#ordered').append('<li  class="item" id=new' + id + '>' + target.text() + '</li>');
-                id = '#' + id;
-                $(id).remove();
+                target.animate({
+                    opacity: 0.0,
+                    paddingRight: '+=80',
+                    paddingTop: '+=200'
+                }, 500, function() {
+                    $('#ordered').append('<li  class="item" id=new' + id + '>' + target.text() + '</li>');
+                    id = '#' + id;
+                    $(id).remove();
+                    }); // end of animate
+
             } //end of first nest if
             else if (target.parent().attr('id') === 'ordered' ){
-                $('#available').append('<li  class="item" id=old' + id + '>' + target.text() + '</li>');
-                id = '#' + id;
-                $(id).remove();
+                target.animate({
+                    opacity: 0.0,
+                    paddingRight: '+=80',
+                    paddingTop: '+=200'
+                }, 500, function() {
+                    $('#available').append('<li  class="item" id=old' + id + '>' + target.text() + '</li>');
+                    id = '#' + id;
+                    $(id).remove();
+                }); //end of animate
             } //end of second nest if
         } // end first IF
     }); //end of doc ready
-
-
-
-
-
 }); // end of doc ready
