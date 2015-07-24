9ead24020a0959a92bf5b19c26bcc1321ade902d
Dar Wright <elack33@gmail.com>
2015-07-23 20:39:32 -0700

    Mostly working, tracking down one bug.

--- StudentWork/DarWright/JavaScript/memory_game/scripts/memory_game.js
+++ StudentWork/DarWright/JavaScript/memory_game/scripts/memory_game.js
@@ -14,7 +14,13 @@ Array.prototype.image_shuffle = function(){
 };
 // creates the game board, 12 spaces with unique ids
 function newGame(){
-    tiles_flipped = 0;
+    clicked_count = 0;
+    clicked_id_1 = '';
+    clicked_id_2 = '';
+    matches = 0;
+    big_click = 0;
+    $('#clicks').text('Clicks: ' + big_click);
+    $('#total').text('Matches: ' + matches);
     var output = '';
     images_array.image_shuffle();
     for(var i = 0; i < images_array.length; i++){
@@ -32,6 +38,7 @@ var big_click = 0;
 function mainGame() {
     //console.log('clicked count is starting?', clicked_count);
     $('.pic').on('click', function() {
+
         if (clicked_count === 0) {
             clicked_id_1 = $(this).attr('id');
             var clicked_src_1 = "images/" + images_array[clicked_id_1] + ".jpg";
@@ -43,89 +50,57 @@ function mainGame() {
             clicked_id_2 = $(this).attr('id');
             var clicked_src_2 = "images/" + images_array[clicked_id_2] + ".jpg";
             $(this).attr('src', clicked_src_2);
-            //console.log('clicked count is 1?', clicked_count);
             clicked_count += 1;
-            //console.log('clicked count is 2?', clicked_count);
             setTimeout(flipOver, 1000);
         }
         big_click +=1;
+        $('#clicks').text('Clicks: ' + big_click);
     });
-    if (matches === 12){
-        alert('You won!');
-        newGame();
-    }
-
 
 }// end of mainGame function
-
 function flipOver() {
     //console.log('flipover!');
     //console.log('id 1:', clicked_id_1, 'id: 2', clicked_id_2);
     console.log('array 1: ', images_array[clicked_id_1], 'array 2:', images_array[clicked_id_2], 'matches: ', matches, 'big_click:', big_click);
+    if (matches < 11) {
+        if (images_array[clicked_id_1] === images_array[clicked_id_2]){
+            matches +=1;
+            clicked_count = 0;
+            $('#total').text('Matches: ' + matches);
+            $('#clicks').text('Clicks: ' + big_click);
+        }
+        else if (images_array[clicked_id_1] !== images_array[clicked_id_2]){
+            var cover_image_1 = "images/gonzo2.jpg";
+            $('#' + clicked_id_1).attr('src', cover_image_1);
+            var cover_image_2 = "images/gonzo2.jpg";
+            $('#' + clicked_id_2).attr('src', cover_image_2);
+            clicked_count = 0;
+            $('#clicks').text('Clicks: ' + big_click);
+        }
+    }
 
-    if (images_array[clicked_id_1] === images_array[clicked_id_2]){
-        matches +=1;
-        //console.log(matches);
-        clicked_count = 0;
+    if (matches >= 11){
+        alert('You won!');
+        newGame();
 
     }
-    else if (images_array[clicked_id_1] !== images_array[clicked_id_2]){
-        //alert('No match!');
-        var cover_image_1 = "images/gonzo2.jpg";
-        $('#' + clicked_id_1).attr('src', cover_image_1);
-        var cover_image_2 = "images/gonzo2.jpg";
-        $('#' + clicked_id_2).attr('src', cover_image_2);
-        //console.log(matches);
-        clicked_count = 0;
-    }
-    
+
 }// end of flipOver function
-
 
 $(document).ready(function() {
     mainGame();
-    //setTimeout(flipOver(clicked_id_1, clicked_id_2),1000);
-    //$.fn.preload = function() {
-    //    this.each(function(){
-    //        $('<img/>')[0].src = this;
-    //    });
-    //};
-    //
-    //$(images_array_preload).preload();
-});//end of doc ready
 
-jQuery.fn.preventDoubleClick = function() {
-    $(this).on('click', function(e){
-        var $el = $(this);
-        if($el.data('clicked')){
-            // Previously clicked, stop actions
-            e.preventDefault();
-            e.stopPropagation();
-        }else{
-            // Mark to ignore next click
-            $el.data('clicked', true);
-            // Unmark after 1 second
-            window.setTimeout(function(){
-                $el.removeData('clicked');
-            }, 1000)
+    $.preloadImages = function() {
+        for (var i = 0; i < arguments.length; i++) {
+            $("<img />").attr("src", arguments[i]);
         }
-    });
-    return this;
-};
+    };
+    $.preloadImages( "images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg",
+        "images/7.jpg", "images/8.jpg", "images/9.jpg", "images/10.jpg", "images/11.jpg", "images/0.jpg");
 
-
-
-
-var images_array_preload = [
-    '<img src="images/1.jpg" width="71" height="90">', '<img src="images/2.jpg" width="71" height="90">',
-    '<img src="images/3.jpg" width="71" height="90">', '<img src="images/4.jpg" width="71" height="90">',
-    '<img src="images/5.jpg" width="71" height="90">', '<img src="images/6.jpg" width="71" height="90">',
-    '<img src="images/7.jpg" width="71" height="90">', '<img src="images/8.jpg" width="71" height="90">',
-    '<img src="images/9.jpg" width="71" height="90">', '<img src="images/10.jpg" width="71" height="90">',
-    '<img src="images/11.jpg" width="71" height="90">', '<img src="images/0.jpg" width="71" height="90">'
-];
-
-
+    $('#total').text('Matches: ' + matches);
+    $('#clicks').text('Clicks: ' + big_click);
+});//end of doc ready
 
 
 
