9165cf41152d6b977096470a08466873d7f2bfa0
Javafab <jgamattingly@gmail.com>
2015-07-28 21:04:07 -0700

    Created Reviews page carousel added comment box area started

--- StudentWork/JayMattingly/Personal_Projects/Project_7-20-15/index.js
+++ StudentWork/JayMattingly/Personal_Projects/Project_7-20-15/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+    //var userComment = document.getElementById('addComment');
+   //function submitComment() {});
 
-
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000
+    });
 
 });
