13afcfd8f4a790a4ecb076f5e2ea726a8d343bd1
Javafab <jgamattingly@gmail.com>
2015-07-29 21:10:49 -0700

    modified comment box, changed photos for review carousel, updated burger prices and extra menu prices

--- StudentWork/JayMattingly/Personal_Projects/Project_7-20-15/index.js
+++ StudentWork/JayMattingly/Personal_Projects/Project_7-20-15/index.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-    //var userComment = document.getElementById('addComment');
-   //function submitComment() {});
 
     //Set the carousel options
     $('#quote-carousel').carousel({
         pause: true,
         interval: 4000
     });
-
 });
