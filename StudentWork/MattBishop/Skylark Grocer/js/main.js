6b78a32bf49298a619f92f14004603fc5efe194b
Matt Bishop <cmbishop@pdx.edu>
2015-07-30 21:18:18 -0700

    Dummy webpage for grocery store for mini Javascript project that includes JQuery and Bootstrap. Last modification - fully functional shopping cart. Next - complete CSS styling and HTML on all pages, create a few more Javascript features. To Do (if time permits) - make the page responsive.

--- StudentWork/MattBishop/Skylark Grocer/js/main.js
+++ StudentWork/MattBishop/Skylark Grocer/js/main.js
@@ -1,109 +1,92 @@
 //Main Page
 
 //Shopping List
-$(document).ready(function(){
 
 
+//function attachCheckboxHandlers() {
+//    var el = document.getElementById('itemChoice');
+//    var items = el.getElementsByTagName('input');
+//    for (var i = 0, len = items.length; i < len; i++) {
+//        if (items[i].type === 'checkbox') {
+//            items[i].onclick = updateTotal;
+//        }
+//    }
+//}
+//
+//function updateTotal() {
+//    var form = this.form;
+//    var val = parseFloat(form['total'].value);
+//    if (this.checked) {
+//        val += parseFloat(this.value);
+//    } else {
+//        val -= parseFloat(this.value);
+//    }
+//
+//    // format val with correct number of decimal places
+//    // and use it to update value of total text box
+//    form.elements['total'].value = formatDecimal(val);
+//}
+//
+////format val to n number of decimal places
+////modified version of Danny Goodman's (JS Bible)
+//function formatDecimal(val, n) {
+//    n = n || 2;
+//    var str = "" + Math.round(parseFloat(val) * Math.pow(10, n));
+//    while (str.length <= n) {
+//        str = "0" + str;
+//    }
+//    var pt = str.length - n;
+//    return str.slice(0, pt) + "." + str.slice(pt);
+//}
+//
+//attachCheckboxHandlers();
 
-    function attachCheckboxHandlers() {
-        var el = document.getElementById('itemChoice');
-        var items = el.getElementsByTagName('input');
-        for (var i = 0, len = items.length; i < len; i++) {
-            if (items[i].type === 'checkbox') {
-                items[i].onclick = updateTotal;
+var cart = [];
+var total = 0;
+
+
+var addItems = document.getElementsByClassName("addCart");
+    for (var i=0; i<addItems.length; i++){
+        addItems[i].addEventListener('click', function(){
+            var item = this.id;
+            var value = parseInt(this.value * 100);
+            total += value;
+            cart.push(item);
+            console.log(total);
+        }, false);
+    }
+
+var removeItems = document.getElementsByClassName("removeCart");
+    for (var i=0; i<removeItems.length; i++){
+        removeItems[i].addEventListener('click', function(){
+            var item = this.id;
+            var index = cart.indexOf(item);
+            console.log(item, index);
+            if (cart.indexOf(item) !== -1){
+                cart.splice(cart.indexOf(item), 1);
+                var value = parseInt(this.value * 100);
+                total -= value;
             }
-        }
+            console.log(total);
+        }, false);
     }
-
-    function updateTotal() {
-        var form = this.form;
-        var val = parseFloat(form.elements['total'].value);
-        if (this.checked) {
-            val += parseFloat(this.value);
-        } else {
-            val -= parseFloat(this.value);
-        }
-
-        // format val with correct number of decimal places
-        // and use it to update value of total text box
-        form.elements['total'].value = formatDecimal(val);
-    }
-
- //format val to n number of decimal places
- //modified version of Danny Goodman's (JS Bible)
-    function formatDecimal(val, n) {
-        n = n || 2;
-        var str = "" + Math.round(parseFloat(val) * Math.pow(10, n));
-        while (str.length <= n) {
-            str = "0" + str;
-        }
-        var pt = str.length - n;
-        return str.slice(0, pt) + "." + str.slice(pt);
-    }
-
-    attachCheckboxHandlers();
-
-    var cart = [];
-
-     $("#filet").change(function () {
-        var item = $("#filet").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#cheese").change(function () {
-        var item = $("#cheese").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-     $("#eggs").change(function () {
-        var item = $("#eggs").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#bread").change(function () {
-        var item = $("#bread").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#green_beans").change(function () {
-        var item = $("#green_beans").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#raspberries").change(function () {
-        var item = $("#raspberries").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#chocolate").change(function () {
-        var item = $("#chocolate").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#coffee").change(function () {
-        var item = $("#coffee").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    $("#wine").change(function () {
-        var item = $("#wine").next().text();
-        cart.push(item);
-        console.log(cart);
-    });
-
-    });
 
 function checkOut() {
-        window.open("checkout.html");
+    console.log(cart);
+    //window.open("checkout.html");
+    var html = "<ol>";
+    var finalTotal = "<p>" + checkOutTotal() + "</p>";
+    for (var i=0; i<cart.length; i++){
+        html += "<li>" + cart[i] + "</li>";
     }
+    html += "</ol>";
+    $("#cartItems").append(html, finalTotal);
+    console.log(html);
+}
 
+function checkOutTotal() {
+    total = "$" + total / 100;
+    return total;
+}
 
 
