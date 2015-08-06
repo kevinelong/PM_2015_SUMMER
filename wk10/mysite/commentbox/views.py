9e1669061287d94ba9beea306a48a91a8ee7fff0
Reina B Abolofia <purple4reina@gmail.com>
2015-08-05 20:01:17 -0700

    Add a view to the commentbox app

--- wk10/mysite/commentbox/views.py
+++ wk10/mysite/commentbox/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import CommentBox
+
+
+def index(request):
+    total_comments = CommentBox.objects.count()
+
+    texts = ''
+    for comment in CommentBox.objects.all():
+        if comment.approved:
+            texts += comment.comment_text
+
+    return HttpResponse(
+        'There are currently {} comment(s). Their text is: {}'.format(
+            total_comments, texts)
+    )
--- wk10/mysite/mysite/urls.py
+++ wk10/mysite/mysite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^commentbox/', include('commentbox.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
