9292afa7c6b9ec5673834dcca91ade3e9e92f39a
Reina B Abolofia <purple4reina@gmail.com>
2015-08-07 20:44:25 -0700

    Create password at user creation

--- wk10/mysite/accounts/forms.py
+++ wk10/mysite/accounts/forms.py
@@ -9,10 +9,26 @@ class NewUserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NewUserForm, self).__init__(*args, **kwargs)
+
+        # change help_text for username
         self.fields['username'].help_text = (
             '<br>A string of letters and numbers, must be '
             'between 6 and 12 characters long.'
         )
+
+        # add two password fields
+        self.fields['passwd1'] = forms.CharField(
+            widget=forms.PasswordInput(),
+            required=True,
+            label='Password',
+        )
+        self.fields['passwd2'] = forms.CharField(
+            widget=forms.PasswordInput(),
+            required=True,
+            label='Reenter password',
+        )
+
+        # require all fields
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
         self.fields['email'].required = True
@@ -25,3 +41,20 @@ class NewUserForm(forms.ModelForm):
         elif not username.isalnum():
             raise forms.ValidationError('Username must be letters and numbers only')
         return username
+
+    def clean_passwd2(self):
+        passwd1 = self.cleaned_data['passwd1']
+        passwd2 = self.cleaned_data['passwd2']
+        if passwd1 != passwd2:
+            raise forms.ValidationError(
+                'Passwords do not match'
+            )
+        return passwd2
+
+    def save(self):
+        password = self.cleaned_data['passwd1']
+        user = super(NewUserForm, self).save()
+
+        # set the user password
+        user.set_password(password)
+        user.save()
