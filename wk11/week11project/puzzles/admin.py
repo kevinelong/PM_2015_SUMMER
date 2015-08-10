6ae616a856914cca367e207d1bc809a991ddfe13
Reina B Abolofia <purple4reina@gmail.com>
2015-08-10 16:49:03 -0700

    Give puzzles an admin page

--- wk11/week11project/puzzles/admin.py
+++ wk11/week11project/puzzles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Puzzle, PuzzleBoard, Clue
+
+
+
+class PuzzleAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Puzzle, PuzzleAdmin)
+
+
+class ClueAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Clue, ClueAdmin)
+
+
+class PuzzleBoardAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PuzzleBoard, PuzzleAdmin)
