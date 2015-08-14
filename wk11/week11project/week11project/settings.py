0e066d61cfa6f36a3ac5613c1dcb140f5d04d19d
Reina B Abolofia <purple4reina@gmail.com>
2015-08-14 12:01:13 -0700

    Create custom 404 page

--- wk11/week11project/week11project/settings.py
+++ wk11/week11project/week11project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'week11project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
