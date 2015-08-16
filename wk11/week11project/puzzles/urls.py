555067024b77f0a4b450848820f72bf1f40142b3
Reina B Abolofia <purple4reina@gmail.com>
2015-08-16 10:39:57 -0700

    Partial solution to Friday Challenge
    Challenge: Create a website for the puzzles app with the following structure.
    Create one template for of the following pages, each using its own view. (Later
    you will work to reduce the number of templates and/or views) Before beginning,
    you can populate your database using a management command that Reina wrote. To
    do so, copy the directory puzzles/management/ to your puzzles directory, then
    run `python manage.py create_puzzles`
    
        Homepage: This page will display a link to all the below pages. It might
        also say welcome.
    
        A page to display all Clues that are used by Puzzles that have a width of
        10 or less. It will provide a link back to the homepage.
    
        A page to display all Clues that are used in Puzzles that are ranked
        “Devilish.” It will also provide a link back to the homepage.
    
        A page to display all Puzzles that have difficulty level “Easy” and whose
        name starts with the letter N. It will also provide a link back to the
        homepage.
    
        A page to display the count of the number of Clues currently in your
        database.  It will also provide a link back to the homepage.
    
        A page to display all Puzzles that are not of size 10 by 10. It will also
        provide a link back to the homepage.
    
        Bonus: Create a page that for a given clue, display all Puzzles that use
        that Clue.
    
        Now that you have created one template for each of these views, see if you
        can reduce the number of templates… and the number of views…

--- wk11/week11project/puzzles/urls.py
+++ wk11/week11project/puzzles/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.home_page, name='puzzles_home'),
     url(r'^total_clues/$', views.clues_count, name='clues_count'),
+    url(r'^clues_width_less_than/(\d+)/$', views.clues_width_less_than, name='clues_width_less_than'),
+    url(r'^devilish_clues/$', views.devilish_clues, name='devilish_clues'),
+    url(r'^clue/$', views.clues_list, name='clues_list'),
+    url(r'^clue/(?P<clue_id>\d+)/$', views.clue_detail, name='clue_detail'),
+    url(r'^puzzle/(?P<puzzle_id>\d+)/$', views.puzzle_detail, name='puzzle_detail'),
 ]
--- wk11/week11project/puzzles/views.py
+++ wk11/week11project/puzzles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 from .models import Clue, Puzzle, PuzzleBoard
 
@@ -9,5 +9,59 @@ def clues_count(request):
         'total_clues.html',
         context={
             'total_clues': total_clues,
+        }
+    )
+
+
+def home_page(request):
+    return render_to_response('home.html')
+
+
+def clues_width_less_than(request, width):
+    clues = Clue.objects.filter(puzzle__puzzle_board__width__lte=width).distinct()
+    return render_to_response(
+        'clues_width_less_than.html',
+        context={
+            'clues': clues,
+            'width': width,
+        }
+    )
+
+
+def devilish_clues(request):
+    clues = Clue.objects.filter(puzzle__difficulty='DE').distinct()
+    return render_to_response(
+        'devilish_clues.html',
+        context={
+            'clues': clues,
+        }
+    )
+
+
+def clue_detail(request, clue_id):
+    clue = get_object_or_404(Clue, id=clue_id)
+    return render_to_response(
+        'clue_detail.html',
+        context={
+            'clue': clue,
+        }
+    )
+
+
+def puzzle_detail(request, puzzle_id):
+    puzzle = get_object_or_404(Puzzle, id=puzzle_id)
+    return render_to_response(
+        'puzzle_detail.html',
+        context={
+            'puzzle': puzzle,
+        }
+    )
+
+
+def clues_list(request):
+    return render_to_response(
+        'clue_list.html',
+        context={
+            'clues': Clue.objects.all(),
         }
     )
