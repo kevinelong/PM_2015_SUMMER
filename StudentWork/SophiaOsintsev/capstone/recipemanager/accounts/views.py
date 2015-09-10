fa11b7bd417e2d962f279b1e12807a793c8309ec
sophiaosintsev <sofica05@gmail.com>
2015-09-09 20:08:26 -0700

    Capstone project in progress
    added password reset

--- StudentWork/SophiaOsintsev/capstone/recipemanager/accounts/views.py
+++ StudentWork/SophiaOsintsev/capstone/recipemanager/accounts/views.py
@@ -1,33 +1,45 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import logout
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.contrib.auth.views import password_reset, password_reset_confirm
+
 
 from .forms import UserForm
 
 
 def register(request):
 
-    context = RequestContext(request)
-    registered = False
     if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            user.username = form.cleaned_data['username']
+            user.backend = 'django.contrib.auth.backends.ModelBackend'
             user.save()
+
+            password = str(form.cleaned_data['password1'])
+            login(request, user)
+
+            return HttpResponseRedirect(reverse('homepage'))
+
     else:
-        user_form = UserForm()
-    return render_to_response(
+        form = UserForm()
+
+    return render(
+        request,
         'register.html',
-        {'user_form': user_form, 'registered': registered},
-        context)
+        context={
+            'form': form,
+        }
+    )
 
-
+@csrf_protect
 def user_login(request):
     context = RequestContext(request)
 
@@ -40,38 +52,19 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect('homepage')
+                return HttpResponseRedirect(reverse('homepage'))
             else:
-                return HttpResponse("Your account is disabled.")
+                return HttpResponse('Your account is disabled')
         else:
-            print "Invalid login details: {}, {}".format(username, password)
-            return HttpResponse("Invalid login details")
+            return HttpResponse('Invalid login details')
 
     else:
-        return render_to_response('registration/login.html', {}, context)
+        return render_to_response('registration/login.html', context)
 
 
 @login_required
 def user_logout(request):
     logout(request)
 
-    return HttpResponseRedirect('/homepage/')
+    return HttpResponseRedirect(reverse('homepage'))
 
-
-def reset(request):
-    return password_reset(request, template_name='registration/password_reset_form.html',
-                          email_template_name='registration/password_reset_email.html',
-                          subject_template_name='registration/password_reset_subject.txt',
-                          post_reset_redirect=reverse('login'))
-
-
-def reset_confirm(request, uidb64=None, token=None):
-    return password_reset_confirm(request,
-                                  template_name='registration/password_reset_confirm.html',
-                                  uidb64=uidb64,
-                                  token=token,
-                                  post_reset_redirect=reverse('reset_complete')
-                                  )
-def reset_complete(request):
-    return render_to_response(
-        'registration/password_reset_complete.html',)
--- StudentWork/SophiaOsintsev/capstone/recipemanager/recipemanager/urls.py
+++ StudentWork/SophiaOsintsev/capstone/recipemanager/recipemanager/urls.py
@@ -21,10 +21,12 @@ from recipes import views as recipes_views
 from accounts import views as accounts_views
 
 urlpatterns = [
-    url(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        accounts_views.reset_confirm, name='reset_confirm'),
-    url(r'^reset/$', accounts_views.reset, name='reset'),
-    url(r'^reset-complete/$', accounts_views.reset_complete, name='reset_complete'),
+    # url(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    #     accounts_views.reset_confirm, name='reset_confirm'),
+    # url(r'^reset/$', accounts_views.reset, name='reset'),
+    # url(r'^reset-complete/$', accounts_views.reset_complete, name='reset_complete'),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'accounts/login/$', auth_views.login),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/$', accounts_views.user_logout, name='logout'),
     url(r'^login/$', accounts_views.user_login, name='login'),
